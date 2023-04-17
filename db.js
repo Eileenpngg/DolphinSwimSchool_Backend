05ea61f3cbb4fc5982495d4728a2324115f381dd
Eileenpngg <eileenpng1843@gmail.com>
2023-04-17 15:03:48 +0800

    'update db.js'

--- db.js
+++ db.js
@@ -1,21 +1,23 @@
-const Pool = require('pg').Pool
+require("dotenv").config();
+const Pool = require("pg").Pool;
 
-const pool= new Pool({
-    user:'eileenpng',
-    password: 'E81271843g!',
-    host:'localhost',
-    port:'5432',
-    database:'swim_app',
+const pool = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect((err) => {
-    if (err) {
-      console.error("connection error", err.stack);
-    } else {
-      console.log(
-        `PostgreSQL Database swim_app connected on Port 5001`
-      );
-    }
-  });
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log(`PostgreSQL Database swim_app connected on Port 5001`);
+  }
+});
 
 module.exports = pool;
